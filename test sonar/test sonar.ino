8f91d2dfe02ee38c3e042e9e21c98fc752b506f6
Alexander <43313549+AlksSAV@users.noreply.github.com>
2024-05-28 17:42:53 +0300

    sketch
    arduino sketch for testing

---
+++ test sonar/test sonar.ino
@@ -0,0 +1,43 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(11,10); // RX, TX
+unsigned char data[4]={};
+float distance;
+
+void setup()
+{
+ Serial.begin(57600);
+ mySerial.begin(9600); 
+}
+
+void loop()
+{
+    do{
+     for(int i=0;i<4;i++)
+     {
+       data[i]=mySerial.read();
+     }
+  }while(mySerial.read()==0xff);
+
+  mySerial.flush();
+
+  if(data[0]==0xff)
+    {
+      int sum;
+      sum=(data[0]+data[1]+data[2])&0x00FF;
+      if(sum==data[3])
+      {
+        distance=(data[1]<<8)+data[2];
+        if(distance>30)
+          {
+           Serial.print("distance=");
+           Serial.print(distance/10);
+           Serial.println("cm");
+          }else 
+             {
+               Serial.println("Below the lower limit");
+             }
+      }else Serial.println("ERROR");
+     }
+     delay(100);
+}
